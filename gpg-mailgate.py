fbda6a6e7265d74bcc7c965ab7927b26af1ce59d
Colin Moller <colin@unixarmy.com>
2013-06-18 18:46:41 -0700

    Rudimentary support for not re-encrypting already encrypted messages

--- gpg-mailgate.py
+++ gpg-mailgate.py
@@ -50,6 +50,8 @@ def encrypt_payload( payload, gpg_to_cmdline ):
 		return payload
 	gpg = GnuPG.GPGEncryptor( cfg['gpg']['keyhome'], gpg_to_cmdline, payload.get_content_charset() )
 	gpg.update( raw_payload )
+	if "-----BEGIN PGP MESSAGE-----" in raw_payload and "-----END PGP MESSAGE-----" in raw_payload:
+		return payload
 	payload.set_payload( gpg.encrypt() )
 	if payload['Content-Disposition']:
 		payload.replace_header( 'Content-Disposition', re.sub(r'filename="([^"]+)"', r'filename="\1.pgp"', payload['Content-Disposition']) )
