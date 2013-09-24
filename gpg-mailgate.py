119b6115574df02358051ca4a488480dc178b144
perennate <admin@perennate.com>
2013-09-23 23:16:06 -0400

    Remove the encrypted to addrs, since that was gpg-mailgate-specific and is no longer used.

--- gpg-mailgate.py
+++ gpg-mailgate.py
@@ -94,14 +94,6 @@ keys = GnuPG.public_keys( cfg['gpg']['keyhome'] )
 gpg_to = list()
 ungpg_to = list()
 
-for enc in encrypted_to_addrs:
-	if enc in keys:
-		gpg_to.append( (enc, enc) )
-	elif cfg.has_key('keymap') and cfg['keymap'].has_key(enc):
-		gpg_to.append( (enc, cfg['keymap'][enc]) )
-	else:
-		ungpg_to.append(enc)
-
 for to in to_addrs:
 	if to in keys:
 		gpg_to.append( (to, to) )
